a6d101b74c323f4d354dadb59dd665e00d37c506
Skr1996 <91935419+Skr1996@users.noreply.github.com>
2023-12-26 13:15:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 def main():
-    if len(sys.argv) != 3:
-        #print("Usage: python app.py <local_folder_path> <bucket_name>")
+    if len(sys.argv) != 2:
+        print("Usage: python app.py <local_folder_path>")
         sys.exit(1)
 
-    local_folder_path, bucket_name = sys.argv[1:]
+    local_folder_path = sys.argv[1]
 
-    # Replace 'your-project-id' with your actual Google Cloud project ID
-    project_id = 'manipalpr-1677473940279'
+    # Replace 'your-project-id' and 'your-bucket-name' with your actual Google Cloud project ID and GCS bucket name
+    project_id = 'your-project-id'
+    bucket_name = 'your-bucket-name'
 
     # Create an instance of GCP_Helper
-    gcp_helper = GCP_Helper(project_id)
+    gcp_helper = GCP_Helper(project_id, bucket_name)
 
     # Upload files from local folder to GCS bucket
-    gcp_helper.upload_files_from_folder(bucket_name, local_folder_path)
+    gcp_helper.upload_files_from_folder(local_folder_path)
 
 if __name__ == "__main__":
     main()
